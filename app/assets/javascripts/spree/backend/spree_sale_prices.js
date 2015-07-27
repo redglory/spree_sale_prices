7c981a4227a95bbbbffd8012505da2d0bcfb7945
redglory <pedropinto.inbox@gmail.com>
2015-07-27 19:02:24 +0100

    refactoring

---
+++ app/assets/javascripts/spree/backend/spree_sale_prices.js
@@ -0,0 +1,10 @@
+//= require spree/backend
+//= require jquery.datetimepicker
+
+$(document).ready(function() {
+  
+  // Start and End Dates with datetimepicker
+  $('#sale_price_start_at').datetimepicker();
+  $('#sale_price_end_at').datetimepicker();
+
+}); // end Ready
