e0573277c0d9f372cfe61720cb8e39d0f95b6219
redglory <pedropinto.inbox@gmail.com>
2018-11-25 19:58:22 +0000

    fix before_filter

--- app/controllers/spree/admin/sale_prices_controller.rb
+++ app/controllers/spree/admin/sale_prices_controller.rb
@@ -2,7 +2,7 @@ module Spree
   module Admin
     class SalePricesController < Spree::Admin::ResourceController
 
-      before_filter :load_product
+      before_action :load_product
 
       respond_to :js, :html
 
